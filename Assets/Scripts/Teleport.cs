630efe8c631ab7581f1c207f0bf80235dbd387ee
QLou895 <qluis895@gmail.com>
2020-05-28 00:16:03 -0400

    teleport_v0

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    //controller input to activate the teleport
+    public GameObject input;
+    public GameObject player;
+
+    //A custome function to activate the teleportation
+
+
+    //A custome function that teleports the player an X amount of space 
+    
+
+    //A custome function to jump behind the opponent
+
+    //the action of teleporting
+    IEnumerable input()
+    {
+        yield return new WaitForSeconds(1);
+        player.transform.position = new Vector2(input.transform.positon.x);
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
