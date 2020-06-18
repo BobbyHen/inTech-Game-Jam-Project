0ffb6ceb9f4ddf0e2458576cc764d933377d98f9
LinkNinja <LinkNinja@users.noreply.github.com>
2020-06-17 22:56:44 -0400

    Initial Commit for Player Controller Branch

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-    private Rigidbody2D rb;
+    
+
+    private Rigidbody2D playerRb;
+    public Animator playerAnim;
+    float horizontalMovement;
+    public float playerSpeed = 10.00f;
+    public float gravityModifier;
+    public float jumpForce = 10.00f;
+    public float dashForce = 0.00f;
+
+
     private Vector2 moveVelocity;
-    public Animator animator;
-    public float jumpForce;
+
     public bool isJumping, isCrouching;
-    float horizontalMove = 0f;
+
     public SpriteRenderer spriteR;
+
     // public Sprite idle, run, crouch, crouch_walk, jump;
     
     private void Awake()
@@ -22,43 +31,75 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        playerRb = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
+        Physics2D.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0f);
-        moveVelocity = moveInput.normalized * speed;
 
-        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;
-        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+        
 
+        //using this to move player velocity
+        //Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), 0f);
+        //moveVelocity = moveInput.normalized * playerSpeed;
+        //horizontalMovement = Input.GetAxisRaw("Horizontal") * playerSpeed;
+        //animation for player.
+        //playerAnim.SetFloat("Speed", Mathf.Abs(horizontalMovement));
         // if(Input.GetKeyDown("d") || Input.GetButtonDown("a"))
         // {   
         //     this.GetComponent<SpriteRenderer>().sprite = run;
         // }
+
+        //Added move method.
+        Move();
         Crouch();
         Flip();
         Jump();
+        Dash();
     }
 
+  /* 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        playerRb.MovePosition(playerRb.position + moveVelocity * Time.fixedDeltaTime);
     }
-
+  */
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump") && !isJumping)
-        {   
+        if(Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             isJumping = true;
-            animator.SetBool("isJumping", true);
+            playerAnim.SetBool("isJumping", true);
             // rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
-            rb.AddForce(Vector2.up * jumpForce);
+            
             Debug.Log("Space key was pressed");
         }
+    }
+    
+    void Move()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        if (horizontalMovement > 0.00f)
+        {
+            playerRb.velocity = new Vector2(horizontalMovement * playerSpeed, playerRb.velocity.y);
+            playerAnim.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        }
+        else if (horizontalMovement < 0.00f)
+        {
+            playerRb.velocity = new Vector2(horizontalMovement * playerSpeed, playerRb.velocity.y);
+            playerAnim.SetFloat("Speed", Mathf.Abs(horizontalMovement));
+        }
+        else
+        {
+            playerRb.velocity = new Vector2(0, playerRb.velocity.y);
+        }
+
+
     }
 
     void Flip()
@@ -80,24 +121,37 @@ public class PlayerController : MonoBehaviour
     // Crouch
     void Crouch()
     {
-        if(Input.GetKey("s")) 
+        if (Input.GetKey(KeyCode.S))
         {
-            animator.SetBool("isCrouch", true);
+            playerAnim.SetBool("isCrouch", true);
             Debug.Log("S was pressed");
         }
         else
         {
-            animator.SetBool("isCrouch", false);
+            playerAnim.SetBool("isCrouch", false);
         }
     }
+
+    void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+           
+            playerRb.AddForce(Vector2.right * 25, ForceMode2D.Impulse);
+            playerAnim.SetBool("Dash_Bool", true);
+            Debug.Log("Shift Is Pressed Character Dashing");
+            
+
+        }
+    }    
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Ground")){
             isJumping = false;
-            animator.SetBool("isJumping", false);
+            playerAnim.SetBool("isJumping", false);
 
-            rb.velocity = Vector2.zero;
+            playerRb.velocity = Vector2.zero;
             Debug.Log("OnCollisionEnter2D Called");
         }
     }
